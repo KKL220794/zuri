97795cdfde8f8546b79536c500df600d25e25ed1
krishna kanhaiya lal <krishnakanhaiyalal@gmail.com>
2024-07-18 18:20:32 +0530

    creds updated

--- frontend/config/firebase.js
+++ frontend/config/firebase.js
@@ -6,13 +6,6 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyD0mnXTVgo4MqAUk7T2DxL9V0cf8v-z6Wg",
-  authDomain: "dementia-mitra.firebaseapp.com",
-  projectId: "dementia-mitra",
-  storageBucket: "dementia-mitra.appspot.com",
-  messagingSenderId: "180409009294",
-  appId: "1:180409009294:web:0f0acff26ed85ed17175df",
-  measurementId: "G-8MR4C5VZWR"
     // apiKey: Constants.expoConfig.extra.apiKey,
     // authDomain: Constants.expoConfig.extra.authDomain,
     // projectId: Constants.expoConfig.extra.projectId,
